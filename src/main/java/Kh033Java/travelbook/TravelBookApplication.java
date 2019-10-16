2727f185572966872aee7ef920844d8d648e1fa2
dyominov <ersamsungc2500>
2019-10-16 17:54:57 +0300

    add gradle with dependency

---
+++ src/main/java/Kh033Java/travelbook/TravelBookApplication.java
@@ -0,0 +1,13 @@
+package Kh033Java.travelbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Kh033Java/travelbook/TravelbookApplicationTests.java
@@ -0,0 +1,16 @@
+package Kh033Java.travelbook;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TravelbookApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
