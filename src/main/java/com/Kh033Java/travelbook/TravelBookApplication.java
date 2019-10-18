46753a4e97c0eef70a8972ae9cd202d018e71090
dyominov <ersamsungc2500>
2019-10-19 01:42:08 +0300

    config and move checkstyle

--- src/main/java/com/Kh033Java/travelbook/TravelBookApplication.java
+++ src/main/java/com/Kh033Java/travelbook/TravelBookApplication.java
@@ -2,12 +2,20 @@ package com.Kh033Java.travelbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * Main application.
+ */
 @SpringBootApplication
+@ComponentScan({"com.Kh033Java.travelbook.*"})
 public class TravelBookApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TravelBookApplication.class, args);
-	}
-
+    /**
+     * Main method.
+     *
+     * @param args - some args
+     */
+    public static void main(final String[] args) {
+        SpringApplication.run(TravelBookApplication.class, args);
+    }
 }
