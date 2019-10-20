b00a5767cdba95c69b9fb349cd83ec74ef6297e4
Ivan Malik <dertalius@gmail.com>
2019-10-21 00:20:13 +0300

    added base for repositories

---
+++ src/main/java/com/Kh033Java/travelbook/repository/PhotoRepository.java
@@ -0,0 +1,10 @@
+package com.Kh033Java.travelbook.repository;
+
+import com.Kh033Java.travelbook.entity.Photo;
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+
+import java.util.List;
+
+public interface PhotoRepository extends Neo4jRepository<Photo, Long> {
+    List<Photo> findAll();
+}
---
+++ src/main/java/com/Kh033Java/travelbook/repository/PlanRepository.java
@@ -0,0 +1,10 @@
+package com.Kh033Java.travelbook.repository;
+
+import com.Kh033Java.travelbook.entity.Plan;
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+
+import java.util.List;
+
+public interface PlanRepository extends Neo4jRepository<Plan, Long> {
+    List<Plan> findAll();
+}
