7f64462e9f4f0e48bb55ddc2cc30bb5d44bac4ea
Djuba <Djubabagdad@gmail.com>
2019-10-22 16:46:30 +0300

    Add Security and Roles

---
+++ src/main/java/dev/userDetails/requestUserDetails/RequestDetail.java
@@ -0,0 +1,25 @@
+package dev.userDetails.requestUserDetails;
+
+public class RequestDetail {
+    private String username;
+    private String password;
+
+    public RequestDetail() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
