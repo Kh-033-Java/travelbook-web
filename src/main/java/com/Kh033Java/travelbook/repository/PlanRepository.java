a355ed2becead805028b1004aaca9d924e242dbf
Ivan Malik <dertalius@gmail.com>
2019-11-05 21:43:54 +0200

    fixed notes

--- src/main/java/com/Kh033Java/travelbook/repository/PlanRepository.java
+++ src/main/java/com/Kh033Java/travelbook/repository/PlanRepository.java
@@ -36,9 +36,8 @@ public interface PlanRepository extends Neo4jRepository<Plan, Long> {
             "return plan")
     List<Plan> findPlansWithFilter(@Param("minBudget") int minBudget, @Param("maxBudget") int maxBudget, @Param("minDate") Date minDate, @Param("maxDate") Date maxDate, @Param("minAmountOfPeople") int minAmountOfPeople, @Param("maxAmountOfPeople") int maxAmountOfPeople, @Param("transportType") String transportType, @Param("startCity") String startCity, @Param("endCity") String endCity);
 
-    @Query("MATCH (plan:Plan) WHERE plan.budgetMin >= {minBudget} AND plan.budgetMax <= {maxBudget}")
+    @Query("MATCH (plan:Plan) WHERE plan.budgetMin >= {minBudget} AND plan.budgetMax <= {maxBudget} return plan")
     List<Plan> findPlansByBudget(@Param("minBudget") int minBudget, @Param("maxBudget") int maxBudget);
-
 
     @Query("MATCH (n:Plan) WHERE n.isPublic=true RETURN n")
     List<Plan> getPublicPlans();
