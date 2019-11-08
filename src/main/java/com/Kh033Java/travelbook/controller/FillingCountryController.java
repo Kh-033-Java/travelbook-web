da04c129faaf5f6ff3c2016c2ebab98a6dc70359
zhe1ezniak <dimazhel88@gmail.com>
2019-11-08 15:11:44 +0200

    delete slash

--- src/main/java/com/Kh033Java/travelbook/controller/FillingCountryController.java
+++ src/main/java/com/Kh033Java/travelbook/controller/FillingCountryController.java
@@ -16,12 +16,12 @@ public class FillingCountryController {
         this.userService = userService;
     }
 
-    @PutMapping(value = "/country/{name}/visit/")
+    @PutMapping(value = "/country/{name}/visit")
     public void addVisitedCountryForUser(@PathVariable String name, @RequestParam String user){
         userService.addVisitedCountry(user, name);
     }
 
-    @PutMapping(value = "/country/{name}/notvisit/")
+    @PutMapping(value = "/country/{name}/notvisit")
     public void deleteVisitedCountryForUser(@PathVariable String name, @RequestParam String user){
         userService.deleteVisitedCountry(user, name);
     }
