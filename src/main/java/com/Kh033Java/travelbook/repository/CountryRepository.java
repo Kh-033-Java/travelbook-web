eb03ebd7e274ef7622d626f9f91450a61111512f
zhe1ezniak <dimazhel88@gmail.com>
2019-11-25 10:44:05 +0200

    commit

--- src/main/java/com/Kh033Java/travelbook/repository/CountryRepository.java
+++ src/main/java/com/Kh033Java/travelbook/repository/CountryRepository.java
@@ -1,13 +1,12 @@
 package com.Kh033Java.travelbook.repository;
 
-import java.util.List;
-
+import com.Kh033Java.travelbook.entity.Country;
 import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.Kh033Java.travelbook.entity.Country;
+import java.util.List;
 
 @Repository
 public interface CountryRepository extends Neo4jRepository<Country, Long> {
@@ -24,5 +23,4 @@ public interface CountryRepository extends Neo4jRepository<Country, Long> {
 
     @Query("match (u:User)-[cr:CREATED_PLAN]->(p:Plan)-[gt:GO_TO]->(city:City)<-[con:CONTAINS]-(country:Country) where u.login={userLogin} return country") 
     List<Country> getCountriesThatUserPlansToVisit(@Param(value = "userLogin") String userLogin);
-    
 }
